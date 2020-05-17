bdbc854eab6e60443d80f6238181bd90ad1aeefd
SimonenkoAlex <alexsim99.99@mail.ru>
2020-05-17 18:38:37 +0600

    ready-made program code

---
+++ WFAEncryption/WFAEncryption/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WFAEncryption
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBtnVigenere = new System.Windows.Forms.RadioButton();
+            this.radioBtnCaesar = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tBCounter = new System.Windows.Forms.TrackBar();
+            this.numLatters = new System.Windows.Forms.Label();
+            this.textKey = new System.Windows.Forms.TextBox();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.keyword = new System.Windows.Forms.Label();
+            this.radioBtnEng = new System.Windows.Forms.RadioButton();
+            this.radioBtnRus = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tBCounter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioBtnVigenere);
+            this.groupBox1.Controls.Add(this.radioBtnCaesar);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(280, 60);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Метод шифрования";
+            // 
+            // radioBtnVigenere
+            // 
+            this.radioBtnVigenere.AutoSize = true;
+            this.radioBtnVigenere.Checked = true;
+            this.radioBtnVigenere.Location = new System.Drawing.Point(135, 26);
+            this.radioBtnVigenere.Name = "radioBtnVigenere";
+            this.radioBtnVigenere.Size = new System.Drawing.Size(139, 23);
+            this.radioBtnVigenere.TabIndex = 0;
+            this.radioBtnVigenere.TabStop = true;
+            this.radioBtnVigenere.Text = "метод Виженера";
+            this.radioBtnVigenere.UseVisualStyleBackColor = true;
+            this.radioBtnVigenere.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioBtnCaesar
+            // 
+            this.radioBtnCaesar.AutoSize = true;
+            this.radioBtnCaesar.Location = new System.Drawing.Point(7, 26);
+            this.radioBtnCaesar.Name = "radioBtnCaesar";
+            this.radioBtnCaesar.Size = new System.Drawing.Size(114, 23);
+            this.radioBtnCaesar.TabIndex = 0;
+            this.radioBtnCaesar.Text = "шифр Цезаря";
+            this.radioBtnCaesar.UseVisualStyleBackColor = true;
+            this.radioBtnCaesar.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioBtnRus);
+            this.groupBox2.Controls.Add(this.radioBtnEng);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(299, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(273, 61);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Язык";
+            // 
+            // tBCounter
+            // 
+            this.tBCounter.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.tBCounter.Location = new System.Drawing.Point(12, 79);
+            this.tBCounter.Maximum = 32;
+            this.tBCounter.Name = "tBCounter";
+            this.tBCounter.Size = new System.Drawing.Size(517, 45);
+            this.tBCounter.TabIndex = 2;
+            this.tBCounter.Visible = false;
+            this.tBCounter.ValueChanged += new System.EventHandler(this.tBCounter_ValueChanged);
+            // 
+            // numLatters
+            // 
+            this.numLatters.AutoSize = true;
+            this.numLatters.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numLatters.Location = new System.Drawing.Point(535, 79);
+            this.numLatters.Name = "numLatters";
+            this.numLatters.Size = new System.Drawing.Size(21, 23);
+            this.numLatters.TabIndex = 3;
+            this.numLatters.Text = "0";
+            this.numLatters.Visible = false;
+            // 
+            // textKey
+            // 
+            this.textKey.Location = new System.Drawing.Point(141, 124);
+            this.textKey.Name = "textKey";
+            this.textKey.Size = new System.Drawing.Size(169, 20);
+            this.textKey.TabIndex = 4;
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDecode.Location = new System.Drawing.Point(447, 115);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(125, 35);
+            this.btnDecode.TabIndex = 5;
+            this.btnDecode.Text = "Расшифровать";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnEncode.Location = new System.Drawing.Point(316, 115);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(125, 35);
+            this.btnEncode.TabIndex = 5;
+            this.btnEncode.Text = "Зашифровать";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // keyword
+            // 
+            this.keyword.AutoSize = true;
+            this.keyword.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.keyword.Location = new System.Drawing.Point(12, 123);
+            this.keyword.Name = "keyword";
+            this.keyword.Size = new System.Drawing.Size(123, 19);
+            this.keyword.TabIndex = 3;
+            this.keyword.Text = "Ключевое слово:";
+            // 
+            // radioBtnEng
+            // 
+            this.radioBtnEng.AutoSize = true;
+            this.radioBtnEng.Location = new System.Drawing.Point(6, 27);
+            this.radioBtnEng.Name = "radioBtnEng";
+            this.radioBtnEng.Size = new System.Drawing.Size(106, 23);
+            this.radioBtnEng.TabIndex = 0;
+            this.radioBtnEng.Text = "английский";
+            this.radioBtnEng.UseVisualStyleBackColor = true;
+            this.radioBtnEng.CheckedChanged += new System.EventHandler(this.radioBtnLanguage_CheckedChanged);
+            // 
+            // radioBtnRus
+            // 
+            this.radioBtnRus.AutoSize = true;
+            this.radioBtnRus.Checked = true;
+            this.radioBtnRus.Location = new System.Drawing.Point(161, 27);
+            this.radioBtnRus.Name = "radioBtnRus";
+            this.radioBtnRus.Size = new System.Drawing.Size(81, 23);
+            this.radioBtnRus.TabIndex = 0;
+            this.radioBtnRus.TabStop = true;
+            this.radioBtnRus.Text = "русский";
+            this.radioBtnRus.UseVisualStyleBackColor = true;
+            this.radioBtnRus.CheckedChanged += new System.EventHandler(this.radioBtnLanguage_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(584, 161);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.textKey);
+            this.Controls.Add(this.keyword);
+            this.Controls.Add(this.numLatters);
+            this.Controls.Add(this.tBCounter);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Симметрическое шифрование";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tBCounter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBtnVigenere;
+        private System.Windows.Forms.RadioButton radioBtnCaesar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TrackBar tBCounter;
+        private System.Windows.Forms.Label numLatters;
+        private System.Windows.Forms.TextBox textKey;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Label keyword;
+        private System.Windows.Forms.RadioButton radioBtnRus;
+        private System.Windows.Forms.RadioButton radioBtnEng;
+    }
+}
+
---
+++ WFAEncryption/WFAEncryption/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAEncryption
+{
+    public partial class MainForm : Form
+    {
+        private static int offset; // сдвиг
+        private static char[] alphabet; // алфавит заданный в виде массива символов
+        private static int N; // мощность алфавита
+        private static int ENG = 26, RUS = 32;
+        private static string method = "Vigenere";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tBCounter_ValueChanged(object sender, EventArgs e)
+        {
+            numLatters.Text = tBCounter.Value.ToString();
+            offset = Convert.ToInt32(numLatters.Text);
+        }
+
+        // реализация метода, шифрующего строку методом Виженера
+        private string Encode_Vigenere(string input, string keyword)
+        {
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
+
+            string result = "";
+            int keyword_index = 0;
+
+            foreach (char symbol in input)
+            {
+                int c = (Array.IndexOf(alphabet, symbol) +
+                    Array.IndexOf(alphabet, keyword[keyword_index])) % N;
+                result += alphabet[c];
+                keyword_index++;
+                if ((keyword_index + 1) == keyword.Length)
+                    keyword_index = 0;
+            }
+            return result;
+        }
+
+        // реализация метода, расшифровывающего строку методом Виженера
+        private string Decode_Vigenere(string input, string keyword)
+        {
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
+
+            string result = "";
+            int keyword_index = 0;
+
+            foreach (char symbol in input)
+            {
+                int p = (Array.IndexOf(alphabet, symbol) + N -
+                    Array.IndexOf(alphabet, keyword[keyword_index])) % N;
+                result += alphabet[p];
+                keyword_index++;
+                if ((keyword_index + 1) == keyword.Length)
+                    keyword_index = 0;
+            }
+            return result;
+        }
+
+        // реализация метода, шифрующего строку методом Цезаря
+        private string Encode_Caesar(string input)
+        {
+            string result = "";
+            char latter;
+            bool flag;
+
+            foreach (char symbol in input)
+            {
+                flag = false; //обработан ли текущий символ
+                if (symbol >= 'A' && symbol <= 'Z')
+                {
+                    latter = Convert.ToChar(symbol + (offset % ENG));
+                    if (latter > 'Z') latter = Convert.ToChar('A' + (latter - 'Z') - 1);
+                    result += latter;
+                    flag = true;
+                }
+                if (symbol >= 'a' && symbol <= 'z')
+                {
+                    latter = Convert.ToChar(symbol + (offset % ENG));
+                    if (latter > 'z') latter = Convert.ToChar('a' + (latter - 'z') - 1);
+                    result += latter;
+                    flag = true;
+                }
+                if (symbol >= 'А' && symbol <= 'Я')
+                {
+                    latter = Convert.ToChar(symbol + (offset % RUS));
+                    if (latter > 'Я') latter = Convert.ToChar('А' + (latter - 'Я') - 1);
+                    result += latter;
+                    flag = true;
+                }
+                if (symbol >= 'а' && symbol <= 'я')
+                {
+                    latter = Convert.ToChar(symbol + (offset % RUS));
+                    if (latter > 'я') latter = Convert.ToChar('а' + (latter - 'я') - 1);
+                    result += latter;
+                    flag = true;
+                }
+                if (!flag) result += symbol;
+                
+            }
+            return result;
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            switch (method)
+            {
+                case "Caesar":
+                    {
+                        string s;
+
+                        StreamReader sr = new StreamReader(@"Text\input.txt", Encoding.GetEncoding(1251));
+                        StreamWriter sw = new StreamWriter(@"Text\output.txt");
+
+                        while (!sr.EndOfStream)
+                        {
+                            s = sr.ReadLine();
+                            sw.WriteLine(Encode_Caesar(s));
+                        }
+
+                        sr.Close();
+                        sw.Close();
+                        break;
+                    }
+                case "Vigenere":
+                    {
+                        if (textKey.Text.Length > 0)
+                        {
+                            string s;
+
+                            StreamReader sr = new StreamReader(@"Text\input.txt");
+                            StreamWriter sw = new StreamWriter(@"Text\output.txt");
+
+                            while (!sr.EndOfStream)
+                            {
+                                s = sr.ReadLine();
+                                sw.WriteLine(Encode_Vigenere(s, textKey.Text));
+                            }
+
+                            sr.Close();
+                            sw.Close();
+                        }
+                        else MessageBox.Show("Введите ключевое слово!");
+                        break;
+                    }
+            }
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            switch (method)
+            {
+                case "Caesar":
+                    {
+                        string s;
+
+                        StreamReader sr = new StreamReader(@"Text\input.txt", Encoding.GetEncoding(1251));
+                        StreamWriter sw = new StreamWriter(@"Text\output.txt");
+
+                        while (!sr.EndOfStream)
+                        {
+                            s = sr.ReadLine();
+                            sw.WriteLine(Decode_Caesar(s));
+                        }
+
+                        sr.Close();
+                        sw.Close();
+                        break;
+                    }
+                case "Vigenere":
+                    {
+                        if (textKey.Text.Length > 0)
+                        {
+                            string s;
+
+                            StreamReader sr = new StreamReader(@"Text\input.txt");
+                            StreamWriter sw = new StreamWriter(@"Text\output.txt");
+
+                            while (!sr.EndOfStream)
+                            {
+                                s = sr.ReadLine();
+                                sw.WriteLine(Decode_Vigenere(s, textKey.Text));
+                            }
+
+                            sr.Close();
+                            sw.Close();
+                        }
+                        else MessageBox.Show("Введите ключевое слово!");
+                        break;
+                    }
+            }
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                if (radioButton.Text == "метод Виженера")
+                {
+                    tBCounter.Visible = false; numLatters.Visible = false;
+                    textKey.Visible = true; keyword.Visible = true;
+                    method = "Vigenere";
+                }
+                if (radioButton.Text == "шифр Цезаря")
+                {
+                    tBCounter.Visible = true; numLatters.Visible = true;
+                    textKey.Visible = false; keyword.Visible = false;
+                    method = "Caesar";
+                }
+            }
+        }
+
+        private void radioBtnLanguage_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                if (radioButton.Text == "английский")
+                {
+                    tBCounter.Maximum = ENG;
+                    alphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                                            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                                            'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '1', '2', '3',
+                                            '4', '5', '6', '7', '8', '9', '0' };
+                    N = alphabet.Length;
+                }
+                if (radioButton.Text == "русский")
+                {
+                    tBCounter.Maximum = RUS;
+                    alphabet = new char[] { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                            'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т',
+                                            'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                            'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6',
+                                            '7', '8', '9', '0' };
+                    N = alphabet.Length;
+                }
+            }
+        }
+
+        // реализация метода, расшифрующего строку методом Цезаря
+        private string Decode_Caesar(string input)
+        {
+            string result = "";
+            char latter;
+            bool flag;
+
+            foreach (char symbol in input)
+            {
+                flag = false; //обработан ли текущий символ
+                if (symbol >= 'A' && symbol <= 'Z')
+                {
+                    latter = Convert.ToChar(symbol - (offset % ENG));
+                    if (latter < 'A') latter = Convert.ToChar('A' - (latter - 'Z') - 1);
+                    result += latter;
+                    flag = true;
+                }
+                if (symbol >= 'a' && symbol <= 'z')
+                {
+                    latter = Convert.ToChar(symbol - (offset % ENG));
+                    if (latter < 'a') latter = Convert.ToChar('a' - (latter - 'z') - 1);
+                    result += latter;
+                    flag = true;
+                }
+                if (symbol >= 'А' && symbol <= 'Я')
+                {
+                    latter = Convert.ToChar(symbol - (offset % RUS));
+                    if (latter < 'А') latter = Convert.ToChar('А' - (latter - 'Я') - 1);
+                    result += latter;
+                    flag = true;
+                }
+                if (symbol >= 'а' && symbol <= 'я')
+                {
+                    latter = Convert.ToChar(symbol - (offset % RUS));
+                    if (latter < 'а') latter = Convert.ToChar('а' - (latter - 'я') - 1);
+                    result += latter;
+                    flag = true;
+                }
+                if (!flag) result += symbol;
+
+            }
+            return result;
+        }
+    }
+}
